a0f5c98920bde0f0ddacd21c8e6d00f06b2466ce
windstormeye <877302410@qq.com>
2019-03-18 23:32:36 +0800

    [v0.0.1] <update> introduction

---
+++ WWDC19Playground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+/*:
+ # Li Style | 黎风
+ 
+ ![brocade of Li nationality](introduction.png)
+ 
+ This introduce brocade of Li nationality game, Li nationality has existed in China for 3,000 years. What is the most attractive is Brocade.
+ 这是介绍黎族的织锦的游戏，黎族在中国有着三千年的历史，其中黎锦最为吸引人，其引领了古代中国的纺织技术的进步。在这个游戏中，我将分两部分与你一同分享黎锦的风采。
+ 
+ 第一部分：你将按照要求利用零件和我特意打造的独特视觉效果拼接出最终的黎锦，感受其中的精华。
+ 
+ 第二部分：我给你准备了一些黎锦中常见的元素构成的组件，它们包括天气、动物、食物等。你可以动手构建出属于自己的黎锦，相信你会喜欢！
+ */
